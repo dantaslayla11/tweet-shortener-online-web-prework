b01c21019fc2dcfa0994490e9c8f2cc131bdf21c
layla dantas <laylatdantas@gmail.com>
2019-05-28 03:10:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,51 @@
+require 'pry'
+
+def dictionary(item)
+ tweets = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4",
+ "four" => "4", 
+ "be" => "b",
+ "you" => "u",
+ "at" => "@",
+ "and" => "&"}
+ 
+ tweets.each do |k,v|
+    if (k==item)
+      return v
+    end  
+ end
+ item
+end 
+
+
+def word_substituter(string)
+   array = string.split(" ")
+   array2 = []
+   array.each do |item|
+     fuck = dictionary(item)
+     item = fuck
+     array2.push(item)
+    end
+    array2.join(" ")
+end
+   
+
+
+
+
+def bulk_tweet_shortener
+end 
+
+
+
+def selective_tweet_shortener
+end 
+
+
+
+def shortened_tweet_truncator
+end 
+
+
+
 # Write your code here.
+
